d2b5f07bff1755b6d01945f90c06e5b234f3979d
develowlper <dev@develowlper.de>
2019-01-26 23:31:18 +0100

    initial

---
+++ src/components/Test/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+/**
+ * super cool test component
+ * @returns react component
+ * @property test:string
+ */
+export const Test = () => {
+  return <div>Test</div>;
+};
